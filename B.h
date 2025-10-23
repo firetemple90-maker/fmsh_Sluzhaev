f664c9d682a7a81f74db446ee58b411fdb5093ae
firetemple90-maker <firetemple90@gmail.com>
2025-10-24 00:35:11 +0700

    Update B.h

--- B.h
+++ B.h
@@ -1,19 +1,16 @@
 #include <iostream>
 using namespace std;
-string xor_operation(string inp_string)
-{
+string xor_operation(string inp_string) {
     char encryption_key = 'A';
     string out_string = "";
     int i = 0;
-    while(i < inp_string.length())
-    {
+    while(i < inp_string.length()) {
         out_string += inp_string[i] ^ encryption_key;
         i++;
     }
     return out_string;
 }
-int main()
-{
+int main() {
     string inp_string;
     getline(cin, inp_string);
     cout << inp_string << endl;
