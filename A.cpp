f1daba862bc15665f549ad46aac318c6b14f61dd
firetemple90-maker <firetemple90@gmail.com>
2025-10-24 00:32:58 +0700

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string encryptCaesar(const string& text, int key) {
+string encryptCaesar(const string text, int key) {
     string result = text;
     for (size_t i = 0; i < result.length(); ++i) {
         if (isalpha(result[i])) {
-            char base = islower(result[i]) ? 'a' : 'A';
+            char base = islower(result[i]) ?'a' : 'A';
             result[i] = (result[i] - base + key) % 26 + base;
         }
     }
     return result;
 }
-string decryptCaesar(const string& text, int key) {
+string decryptCaesar(const string text, int key) {
     return encryptCaesar(text, 26 - key);
 }
-
 int main() {
-    setlocale(LC_ALL, "");
     string inputText;
     int key;
     getline(cin, inputText);
     cin >> key;
-    cin.ignore();
     string encrypted = encryptCaesar(inputText, key);
     string decrypted = decryptCaesar(encrypted, key);
     cout << inputText << endl;
